1209cb7e59152ed07a990b0ab7fa4917ea260323
Brad Cornes <bradlc41@gmail.com>
2022-01-11 15:30:07 +0000

    Tidy up tests with `test` helper

--- test.mjs
+++ test.mjs
@@ -13,76 +13,53 @@ function format(str, parser, options = {}) {
   })
 }
 
+let yes = '__YES__'
+let no = '__NO__'
+let testClassName = 'sm:p-0 p-0'
+let testClassNameSorted = 'p-0 sm:p-0'
+
+function test(strings, ...values) {
+  let input = ''
+  strings.forEach((string, i) => {
+    input += string + (values[i] ? testClassName : '')
+  })
+
+  let output = ''
+  strings.forEach((string, i) => {
+    let value = values[i] || ''
+    if (value === yes) value = testClassNameSorted
+    else if (value === no) value = testClassName
+    output += string + value
+  })
+
+  return [input, output]
+}
+
 const html = [
-  ['<div class="sm:p-0 p-0"></div>', '<div class="p-0 sm:p-0"></div>'],
-  ['<div not-class="sm:p-0 p-0"></div>', '<div not-class="sm:p-0 p-0"></div>'],
+  test`<div class="${yes}"></div>`,
+  test`<div not-class="${no}"></div>`,
 ]
 
-const css = [
-  ['@apply sm:p-0 p-0;', '@apply p-0 sm:p-0;'],
-  ['@not-apply sm:p-0 p-0;', '@not-apply sm:p-0 p-0;'],
-]
+const css = [test`@apply ${yes};`, test`@not-apply ${no};`]
 
 const javascript = [
-  [
-    ';<div className="sm:p-0 p-0"></div>',
-    ';<div className="p-0 sm:p-0"></div>',
-  ],
-  [
-    ";<div className={`sm:p-0 p-0 ${'sm:p-0 p-0'} ${'sm:p-0 p-0' ? 'sm:p-0 p-0' : 'sm:p-0 p-0'}`}></div>",
-    ";<div className={`p-0 sm:p-0 ${'p-0 sm:p-0'} ${'sm:p-0 p-0' ? 'p-0 sm:p-0' : 'p-0 sm:p-0'}`}></div>",
-  ],
-  [
-    ";<div className={'sm:p-0 p-0'}></div>",
-    ";<div className={'p-0 sm:p-0'}></div>",
-  ],
-  [
-    ";<div className={'sm:p-0 p-0' + 'sm:p-0 p-0'}></div>",
-    ";<div className={'p-0 sm:p-0' + 'p-0 sm:p-0'}></div>",
-  ],
-  [
-    ";<div className={'sm:p-0 p-0' ? 'sm:p-0 p-0' + 'sm:p-0 p-0' : 'sm:p-0 p-0'}></div>",
-    ";<div className={'sm:p-0 p-0' ? 'p-0 sm:p-0' + 'p-0 sm:p-0' : 'p-0 sm:p-0'}></div>",
-  ],
-  [
-    ";<div className={clsx('sm:p-0 p-0', ['sm:p-0 p-0'])}></div>",
-    ";<div className={clsx('p-0 sm:p-0', ['p-0 sm:p-0'])}></div>",
-  ],
-  [
-    ";<div className={clsx({ 'sm:p-0 p-0': 'sm:p-0 p-0' })}></div>",
-    ";<div className={clsx({ 'sm:p-0 p-0': 'sm:p-0 p-0' })}></div>",
-  ],
-  [
-    ";<div className={{ 'sm:p-0 p-0': 'sm:p-0 p-0' }['sm:p-0 p-0']}></div>",
-    ";<div className={{ 'sm:p-0 p-0': 'sm:p-0 p-0' }['sm:p-0 p-0']}></div>",
-  ],
+  test`;<div className="${yes}"></div>`,
+  test`;<div className={\`${yes} \${'${yes}'} \${'${no}' ? '${yes}' : '${yes}'}\`}></div>`,
+  test`;<div className={'${yes}'}></div>`,
+  test`;<div className={'${yes}' + '${yes}'}></div>`,
+  test`;<div className={'${no}' ? '${yes}' + '${yes}' : '${yes}'}></div>`,
+  test`;<div className={clsx('${yes}', ['${yes}'])}></div>`,
+  test`;<div className={clsx({ '${no}': '${no}' })}></div>`,
+  test`;<div className={{ '${no}': '${no}' }['${no}']}></div>`,
 ]
 
 const vue = [
-  [
-    '<div :class="\'sm:p-0 p-0\'"></div>',
-    '<div :class="\'p-0 sm:p-0\'"></div>',
-  ],
-  [
-    "<div :class=\"'sm:p-0 p-0' + 'sm:p-0 p-0'\"></div>",
-    "<div :class=\"'p-0 sm:p-0' + 'p-0 sm:p-0'\"></div>",
-  ],
-  [
-    '<div :class="[\'sm:p-0 p-0\']"></div>',
-    '<div :class="[\'p-0 sm:p-0\']"></div>',
-  ],
-  [
-    "<div :class=\"['sm:p-0 p-0' ? 'sm:p-0 p-0' : 'sm:p-0 p-0']\"></div>",
-    "<div :class=\"['sm:p-0 p-0' ? 'p-0 sm:p-0' : 'p-0 sm:p-0']\"></div>",
-  ],
-  [
-    "<div :class=\"{ 'sm:p-0 p-0': true, 'sm:p-0 p-0': 'sm:p-0 p-0' }\"></div>",
-    "<div :class=\"{ 'p-0 sm:p-0': true, 'p-0 sm:p-0': 'sm:p-0 p-0' }\"></div>",
-  ],
-  [
-    "<div :class=\"{ 'sm:p-0 p-0': 'sm:p-0 p-0' }['sm:p-0 p-0']\"></div>",
-    "<div :class=\"{ 'sm:p-0 p-0': 'sm:p-0 p-0' }['sm:p-0 p-0']\"></div>",
-  ],
+  test`<div :class="'${yes}'"></div>`,
+  test`<div :class="'${yes}' + '${yes}'"></div>`,
+  test`<div :class="['${yes}']"></div>`,
+  test`<div :class="['${no}' ? '${yes}' : '${yes}']"></div>`,
+  test`<div :class="{ '${yes}': true, '${yes}': '${no}' }"></div>`,
+  test`<div :class="{ '${no}': '${no}' }['${no}']"></div>`,
 ]
 
 const tests = {

10a2d1c13aa9327b6989e09d7fb0128aa1f5056f
Brad Cornes <bradlc41@gmail.com>
2022-01-20 18:44:30 +0000

    Add dependency licenses

---
+++ scripts/copy-licenses.js
@@ -0,0 +1,32 @@
+const checker = require('license-checker')
+const { devDependencies } = require('../package.json')
+const fs = require('fs')
+const path = require('path')
+
+let exclude = [
+  '@tailwindcss/line-clamp',
+  'cpy-cli',
+  'esbuild',
+  'jest',
+  'license-checker',
+  'prettier',
+  'rimraf',
+]
+
+checker.init({ start: path.resolve(__dirname, '..') }, (_err, packages) => {
+  for (let key in packages) {
+    let name = key.split(/(?<=.)@/)[0]
+    if (
+      name in devDependencies &&
+      !exclude.includes(name) &&
+      packages[key].licenseFile
+    ) {
+      let dir = path.resolve(__dirname, '../dist/licenses', name)
+      fs.mkdirSync(dir, { recursive: true })
+      fs.copyFileSync(
+        packages[key].licenseFile,
+        path.resolve(dir, path.basename(packages[key].licenseFile))
+      )
+    }
+  }
+})

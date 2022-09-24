781699e73cec2ecee45517a9115b4eb1e3b89db8
Matthew Scholta <matthew.scholta@shiftsmart.com>
2022-09-24 13:17:52 -0700

    feat: expanding attributes to include "style" for use in React Native apps

--- src/index.js
+++ src/index.js
@@ -404,7 +404,8 @@ function transformJavaScript(ast, { env }) {
       if (!node.value) {
         return
       }
-      if (['class', 'className'].includes(node.name.name)) {
+
+      if (['class', 'className', 'style'].includes(node.name.name)) {
         if (isStringLiteral(node.value)) {
           sortStringLiteral(node.value, { env })
         } else if (node.value.type === 'JSXExpressionContainer') {

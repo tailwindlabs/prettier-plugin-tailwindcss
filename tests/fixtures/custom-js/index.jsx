c3903d7de544a066ce14a0f2fc92d670265c6cb7
Jordan Pittman <jordan@cryptica.me>
2023-05-06 17:12:01 -0400

    Fix CS

--- tests/fixtures/custom-js/index.jsx
+++ tests/fixtures/custom-js/index.jsx
@@ -1,6 +1,6 @@
 const a = sortMeFn("sm:p-1 p-2");
 const b = sortMeFn({
-  foo: "sm:p-1 p-2"
+  foo: "sm:p-1 p-2",
 });
 
 const c = dontSortFn("sm:p-1 p-2");
@@ -8,4 +8,4 @@ const d = sortMeTemplate`sm:p-1 p-2`;
 const e = dontSortMeTemplate`sm:p-1 p-2`;
 
 const A = (props) => <div className={props.sortMe} />;
-const B = () => <A sortMe="sm:p-1 p-2" dontSort="sm:p-1 p-2" />
+const B = () => <A sortMe="sm:p-1 p-2" dontSort="sm:p-1 p-2" />;

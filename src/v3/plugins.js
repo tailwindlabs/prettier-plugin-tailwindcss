eaf3b7e0f463de776079a5d3564d2657e5ab7c7c
Jordan Pittman <jordan@cryptica.me>
2023-07-25 13:03:32 -0400

    wip

--- src/v3/plugins.js
+++ src/v3/plugins.js
@@ -137,10 +137,10 @@ async function loadBuiltinPlugins() {
  */
 async function loadThirdPartyPlugins() {
   // Commented out plugins do not currently work with Prettier v3.0
-  let [astro, liquid, marko, svelte] = await Promise.all([
+  let [astro, liquid, svelte] = await Promise.all([
     loadIfExistsESM('prettier-plugin-astro'),
     loadIfExistsESM('@shopify/prettier-plugin-liquid'),
-    loadIfExistsESM('prettier-plugin-marko'),
+    // loadIfExistsESM('prettier-plugin-marko'),
     // loadIfExistsESM('prettier-plugin-twig-melody'),
     // loadIfExistsESM('@prettier/plugin-pug'),
     loadIfExistsESM('prettier-plugin-svelte'),
@@ -150,7 +150,7 @@ async function loadThirdPartyPlugins() {
     parsers: {
       ...astro.parsers,
       ...liquid.parsers,
-      ...marko.parsers,
+      // ...marko.parsers,
       // ...melody.parsers,
       // ...pug.parsers,
       ...svelte.parsers,

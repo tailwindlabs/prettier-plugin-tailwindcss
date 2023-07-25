ef1fd555d7afc8c0d5a3d20dcfb798fbc0cb3623
Jordan Pittman <jordan@cryptica.me>
2023-07-25 10:34:22 -0400

    Split into CJS and ESM builds
    The CJS build only supports Prettier v2 and the ESM build only supports Prettier v3. This works out fine because Prettier v3 loads plugins using `import()` and Prettier v2 loads plugins using `require()`.

--- src/index.js
+++
@@ -1,8 +0,0 @@
-// @ts-check
-import prettier from "prettier"
-
-if (prettier.version.startsWith('2.')) {
-  module.exports = require('./v2/plugin.js')
-} else {
-  module.exports = require('./v3/plugin.js')
-}

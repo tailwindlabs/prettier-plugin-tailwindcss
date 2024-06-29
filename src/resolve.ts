f99b9ff0e4e772bdbaab56602ce4480630efe11a
Jordan Pittman <jordan@cryptica.me>
2024-06-28 20:48:45 -0400

    Add cache for known plugins
    We have to call `require.resolve(…)` ~10 times per call to `parse(…)`. These calls generally take microseconds however if you call `prettier.format(…)` several thousand times the times add up. It also adds up for embedded documents which call `parse(…)` a large number of times.
    
    This adds a cache for these calls because generally only a few will ever be installed and Node.js will not cache misses internally.

---
+++ src/resolve.ts
@@ -0,0 +1,47 @@
+import { createRequire as req } from 'node:module'
+import { expiringMap } from './expiring-map'
+
+const localRequire = req(import.meta.url)
+
+// This is a long-lived cache for resolved modules whether they exist or not
+// Because we're compatible with a large number of plugins, we need to check
+// for the existence of a module before attempting to import it. This cache
+// is used to mitigate the cost of that check because Node.js does not cache
+// failed module resolutions making repeated checks very expensive.
+const resolveCache = expiringMap<string, string | null>(30_000)
+
+export function resolveIn(id: string, paths: string[]) {
+  return localRequire.resolve(id, {
+    paths,
+  })
+}
+
+export function maybeResolve(name: string) {
+  let modpath = resolveCache.get(name)
+
+  if (modpath === undefined) {
+    modpath = freshMaybeResolve(name)
+    resolveCache.set(name, modpath)
+  }
+
+  return modpath
+}
+
+export async function loadIfExists<T>(name: string): Promise<T | null> {
+  let modpath = maybeResolve(name)
+
+  if (modpath) {
+    let mod = await import(name)
+    return mod.default ?? mod
+  }
+
+  return null
+}
+
+function freshMaybeResolve(name: string) {
+  try {
+    return localRequire.resolve(name)
+  } catch (err) {
+    return null
+  }
+}

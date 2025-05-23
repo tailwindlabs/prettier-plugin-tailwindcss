9b5ca27351c94ac48b91919586a2f0291354be01
Jordan Pittman <jordan@cryptica.me>
2025-05-23 18:39:10 -0400

    Add tests

--- tests/fixtures/custom-jsx/index.jsx
+++ tests/fixtures/custom-jsx/index.jsx
@@ -22,6 +22,10 @@ const s = tw(Foo)('sm:p-1 p-2');
 const t = tw(Foo)(Bar)('sm:p-1 p-2');
 const u = no(Foo)('sm:p-1 p-2');
 const v = no.tw(Foo)('sm:p-1 p-2');
+const w = tw.div(Foo)`sm:p-1 p-2`;
+const x = tw(Foo).div`sm:p-1 p-2`;
+const y = no.tw(Foo)`sm:p-1 p-2`;
+const z = no(Foo).tw`sm:p-1 p-2`;
 
 const A = (props) => <div className={props.sortMe} />;
 const B = () => <A sortMe="sm:p-1 p-2" dontSort="sm:p-1 p-2" />;
--- tests/fixtures/custom-jsx/output.jsx
+++ tests/fixtures/custom-jsx/output.jsx
@@ -22,6 +22,10 @@ const s = tw(Foo)("p-2 sm:p-1");
 const t = tw(Foo)(Bar)("p-2 sm:p-1");
 const u = no(Foo)("sm:p-1 p-2");
 const v = no.tw(Foo)("sm:p-1 p-2");
+const w = tw.div(Foo)`p-2 sm:p-1`;
+const x = tw(Foo).div`p-2 sm:p-1`;
+const y = no.tw(Foo)`sm:p-1 p-2`;
+const z = no(Foo).tw`sm:p-1 p-2`;
 
 const A = (props) => <div className={props.sortMe} />;
 const B = () => <A sortMe="p-2 sm:p-1" dontSort="sm:p-1 p-2" />;

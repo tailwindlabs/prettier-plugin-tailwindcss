26566b65006a67e1b6a99d8b64f979e17c3fd0c6
Jordan Pittman <jordan@cryptica.me>
2025-10-16 07:15:15 -0400

    Update tests
    Some of these would’ve caught a bug if written correctly 🤦‍♂️

--- tests/format.test.ts
+++ tests/format.test.ts
@@ -193,21 +193,21 @@ describe('regex matching', () => {
   })
 
   test('works with Vue dynamic bindings', async ({ expect }) => {
-    let result = await format('<div :data-classes="sm:p-0 p-0"></div>', {
+    let result = await format('<div :data-classes="`sm:p-0 p-0`"></div>', {
       parser: 'vue',
       tailwindAttributes: ['/data-.*/'],
     })
 
-    expect(result).toEqual('<div :data-classes="p-0 sm:p-0"></div>')
+    expect(result).toEqual('<div :data-classes="`p-0 sm:p-0`"></div>')
   })
 
   test('works with Angular property bindings', async ({ expect }) => {
-    let result = await format('<div [dataClasses]="sm:p-0 p-0"></div>', {
+    let result = await format('<div [dataClasses]="`sm:p-0 p-0`"></div>', {
       parser: 'angular',
       tailwindAttributes: ['/data.*/i'],
     })
 
-    expect(result).toEqual('<div [dataClasses]="p-0 sm:p-0"></div>')
+    expect(result).toEqual('<div [dataClasses]="`p-0 sm:p-0`"></div>')
   })
 
   test('invalid regex patterns do nothing', async ({ expect }) => {
@@ -218,25 +218,43 @@ describe('regex matching', () => {
     expect(result).toEqual('<div data-test="sm:p-0 p-0"></div>')
   })
 
+  test('dynamic attributes are not matched as static attributes', async ({ expect }) => {
+    let result = await format(`<div :custom-class="['sm:p-0 flex underline p-0']"></div>`, {
+      parser: 'vue',
+      tailwindAttributes: ['/.*-class/'],
+    })
+
+    expect(result).toEqual(`<div :custom-class="['flex p-0 underline sm:p-0']"></div>`)
+  })
+
+  test('dynamic attributes are not matched as static attributes (2)', async ({ expect }) => {
+    let result = await format(`<div :custom-class="['sm:p-0 flex underline p-0']"></div>`, {
+      parser: 'vue',
+      tailwindAttributes: ['/:custom-class/'],
+    })
+
+    expect(result).toEqual(`<div :custom-class="['sm:p-0 flex underline p-0']"></div>`)
+  })
+
   // These tests pass but that is a side-effect of the implementation
   // If these change in the future to no longer pass that is a good thing
   describe('dynamic attribute matching quirks', () => {
     test('Vue', async ({ expect }) => {
-      let result = await format('<div ::data-classes="sm:p-0 p-0"></div>', {
+      let result = await format('<div ::data-classes="`sm:p-0 p-0`"></div>', {
         parser: 'vue',
         tailwindAttributes: ['/:data-.*/'],
       })
 
-      expect(result).toEqual('<div ::data-classes="p-0 sm:p-0"></div>')
+      expect(result).toEqual('<div ::data-classes="`p-0 sm:p-0`"></div>')
     })
 
     test('Angular', async ({ expect }) => {
-      let result = await format('<div [[dataClasses]]="sm:p-0 p-0"></div>', {
+      let result = await format('<div [[dataClasses]]="`sm:p-0 p-0`"></div>', {
         parser: 'angular',
         tailwindAttributes: ['/\\[data.*\\]/i'],
       })
 
-      expect(result).toEqual('<div [[dataClasses]]="p-0 sm:p-0"></div>')
+      expect(result).toEqual('<div [[dataClasses]]="`p-0 sm:p-0`"></div>')
     })
   })
 })
